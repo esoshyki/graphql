e0002c0f346eb0e59d45743e0ace692ecd41dc64
esoshyki <esoshyki@yandex.by>
2020-06-10 15:51:11 +0300

    add mongo db but not working

--- schema/schema.js
+++ schema/schema.js
@@ -2,23 +2,33 @@ const graphql = require('graphql');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList } = graphql;
 
-const movies = [
-  {id : "1", name: 'Pulp Fiction', genre: 'Crime', directorId: "1"},
-  {id : "2", name: '1984', genre: 'Sci-Fi', directorId: "2"},
-  {id : "3", name: 'V For Vendetta', genre: 'Sct-Fi-Triller', directorId: "4"},
-  {id : "4", name: 'Snatch', genre: 'Crime-Comedy', directorId: "3"}, 
-  {id : "5", name: 'Reselvors Dogs', genre: 'Crime', directorId: "1"},
-  {id : "6", name: 'The hatefull Eight', genre: 'Crime', directorId: "1"},
-  {id : "7", name: 'Inglourses Basters', genre: 'Crime', directorId: "1"},
-  {id : "8", name: 'Lock, smock and two Smoking Barrels', genre: 'Crime-Comedy', directorId: "4"}, 
-]
+const Movies = require('../server/models/movie');
+const Directors = require('../server/models/director');
 
-const directors = [
-  {id : "1", name: 'Quantin Tarantino', age: 55},
-  {id : "2", name: 'Michael Redford', age: 72},
-  {id : "3", name: 'James McTeague', age: 51},
-  {id : "4", name: 'Guy Ritchie', age: 50}, 
-]
+// const directorsJSON = [
+//   { name: "Quantin Tarantino", age: 55},// 5ee09fb2c41874ceac40f69c
+//   { name: 'Michael Redford', age: 72}, //5ee09ff1c41874ceac40f69e"
+//   { name: 'James McTeague', age: 51}, //5ee09fb2c41874ceac40f69c  
+//   { name: 'Guy Ritchie', age: 50}, //5ee0a4e4c41874ceac40f6a3
+// ]
+
+// const movies = [
+//   {id : "1", name: 'Pulp Fiction', genre: 'Crime', directorId: "1"},
+//   {id : "2", name: '1984', genre: 'Sci-Fi', directorId: "2"},
+//   {id : "3", name: 'V For Vendetta', genre: 'Sct-Fi-Triller', directorId: "3"},
+//   {id : "4", name: 'Snatch', genre: 'Crime-Comedy', directorId: "4"}, 
+//   {id : "5", name: 'Reselvors Dogs', genre: 'Crime', directorId: "1"},
+//   {id : "6", name: 'The hatefull Eight', genre: 'Crime', directorId: "1"},
+//   {id : "7", name: 'Inglourses Basters', genre: 'Crime', directorId: "1"},
+//   {id : "8", name: 'Lock, smock and two Smoking Barrels', genre: 'Crime-Comedy', directorId: "4"}, 
+// ]
+
+// const directors = [
+//   {id : "1", name: 'Quantin Tarantino', age: 55},
+//   {id : "2", name: 'Michael Redford', age: 72},
+//   {id : "3", name: 'James McTeague', age: 51},
+//   {id : "4", name: 'Guy Ritchie', age: 50}, 
+// ]
 
 const MovieType = new GraphQLObjectType({
   name: 'Movie',
@@ -29,7 +39,7 @@ const MovieType = new GraphQLObjectType({
     director: {
       type: DirectorType,
       resolve(parent, args) {
-        return directors.find(director => director.id == parent.directorId)
+        return Directors.findById(parent.directorId)
       }
     }
   }),
@@ -44,7 +54,9 @@ const DirectorType = new GraphQLObjectType({
     movies: { 
       type: GraphQLList(MovieType) ,
       resolve(parent, args) {
-        return movies.filter(movie => movie.directorId === parent.id)
+        return Movies.find({
+          directorId: parent.id
+        })
       },
     },
   }),
@@ -57,26 +69,27 @@ const Query = new GraphQLObjectType({
       type: MovieType,
       args: { id: { type: GraphQLID }},
       resolve(parent, args) {
-        return movies.find(movie => movie.id == args.id)
+        console.log('id', args.id)
+        Movies.findById(args.id)
       }
     },
-    director: {
+    director: { 
       type: DirectorType,
       args: { id: { type: GraphQLID }},
       resolve(parent, args) {
-        return directors.find(director => director.id == args.id)
+        Directors.findById(args.id)
       }
     },
     movies: {
       type: new GraphQLList(MovieType),
       resolve(parent, args) {
-        return movies
+        Movies.find({})
       }
     },
     directors: {
       type: new GraphQLList(DirectorType),
       resolve(parent, args) {
-        return directors
+        Directors.find({})
       }
     }
   }
--- server/app.js
+++ server/app.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('../schema/schema')
+const mongoose = require('mongoose');
+const dbString = 'mongodb+srv://shyki:3guv915fQepTyzDH@cluster0-uk0mn.mongodb.net/?retryWrites=true&w=majority'
 
 const app = express();
 const PORT = 3005
+
+mongoose.connect(dbString,  { useNewUrlParser: true, useUnifiedTopology: true, dbName: "graphql-tutorial" })
 
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
 }))
+
+const dbConnection = mongoose.connection;
+dbConnection.on('error', err => console.log(err))
+dbConnection.once('open', () => console.log('db connected'))
 
 app.listen(PORT, err => {
   err ? console.log(err) : console.log(`app listen on ${PORT}`)
